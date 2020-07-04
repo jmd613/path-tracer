8c1b3810d0bbea0f485e63bdf7cea2bf88f3ee12
jmd613 <jasond@dde-inc.com>
2020-07-04 14:48:18 -0400

    image cleanup

--- src/gfx/Image.cc
+++ src/gfx/Image.cc
@@ -6,11 +6,11 @@
 namespace gfx {
 
 Image::Image(size_t width, size_t height) :
-   pixels_(std::vector<Pixel>(width * height, Pixel())),
-   width_(width), height_(height)
+   pixels_(std::vector<Pixel>(width * height, Pixel())), width_(width),
+   height_(height)
 {}
 
-void WriteImage(const Image& img, const std::string &path)
+void WriteImage(const Image &img, const std::string &path)
 {
    std::ofstream file;
    file.open(path, std::ofstream::trunc);
@@ -21,13 +21,10 @@ void WriteImage(const Image& img, const std::string &path)
    // PPM Header
    file << "P3\n" << width << ' ' << height << "\n255\n";
 
-   for (size_t y = 0; y < height; ++y)
+   for (size_t idx = 0; idx  < width * height; ++idx)
    {
-      for (size_t x = 0; x < width; ++x)
-      {
-         auto &color = img.GetColor(x, y);
-         file << +color.r << ' ' << +color.g << ' ' << +color.b << '\n';
-      }
+      auto pix = img.GetPixel(idx);
+      file << +pix.r << ' ' << +pix.g << ' ' << +pix.b << '\n';
    }
 }
 
--- src/gfx/Image.h
+++ src/gfx/Image.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <string>
 #include <vector>
 
@@ -14,18 +15,30 @@ class Image
 public:
    Image(size_t width, size_t height);
 
+   void SetPixel(const Pixel &pixel, size_t idx)
+   {
+      assert(idx < width_ * height_);
+      pixels_.at(idx) = pixel;
+   }
+
    void SetPixel(const Pixel &pixel, size_t x, size_t y)
    {
       pixels_.at(width_ * y + x) = pixel;
    }
 
-   const Pixel &GetColor(size_t x, size_t y) const
+   [[nodiscard]] const Pixel &GetPixel(size_t idx) const
+   {
+      assert(idx < width_ * height_);
+      return pixels_.at(idx);
+   }
+
+   [[nodiscard]] const Pixel &GetPixel(size_t x, size_t y) const
    {
       return pixels_.at(width_ * y + x);
    }
 
-   size_t GetWidth() const { return width_; }
-   size_t GetHeight() const { return height_; }
+   [[nodiscard]] size_t GetWidth() const { return width_; }
+   [[nodiscard]] size_t GetHeight() const { return height_; }
 
 private:
    std::vector<Pixel> pixels_;
