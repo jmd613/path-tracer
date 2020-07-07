ca07110522e1b387680185665e30e96de19b5622
Jason Deal <jasond@dde-inc.com>
2020-07-06 23:36:42 -0400

    fixed comment typo

--- src/Scene.cc
+++ src/Scene.cc
@@ -8,7 +8,7 @@ std::optional<HitRecord> Scene::Hit(const math::Ray &ray, double min_dist,
    {
       if (auto hit = obj->Hit(ray, min_dist, max_dist))
       {
-         max_dist = hit->dist;   // minimum dist is now the distance of the hit
+         max_dist = hit->dist;   // maximum dist is now the distance of the hit
          closest_hit = hit;
       }
    }
