817bc64d8fb2274313f1ec30acc6d2b6eacab45b
jmd613 <jasond@dde-inc.com>
2020-07-06 23:27:32 -0400

    virtual destructor and hit record mod

--- src/IHittable.h
+++ src/IHittable.h
@@ -9,11 +9,13 @@ struct HitRecord
 {
    math::Vec3 point;
    math::Vec3 normal;
+   double dist;
 };
 
 class IHittable
 {
 public:
+   virtual ~IHittable() = default;
    virtual std::optional<HitRecord> Hit(const math::Ray &ray, double min_dist,
                                         double max_dist) const = 0;
 };
