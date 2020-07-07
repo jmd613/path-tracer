601b63b9298bb9e724734b899a3da8d77f0f6929
jmd613 <jasond@dde-inc.com>
2020-07-06 21:40:06 -0400

    Ray posat func

--- src/math/Ray.h
+++ src/math/Ray.h
@@ -9,11 +9,13 @@ class Ray
 public:
    Ray() = default;
    Ray(const Vec3 &origin, const Vec3 &direction) :
-      origin_(origin), direction_(direction)
+      origin_(origin), direction_(direction.Normal())
    {}
 
    Vec3 GetOrigin() const { return origin_; }
    Vec3 GetDirection() const { return direction_; }
+
+   Vec3 PositionAt(double dist) const { return origin_ + (direction_ * dist); }
 
 private:
    Vec3 origin_, direction_;
